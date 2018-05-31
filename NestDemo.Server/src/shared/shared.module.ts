0cabb5b591829b8c2abea632ca27f48969ffc7e8
Chau Tran <ctch5@mail.umsl.edu>
2018-05-31 15:16:32 -0500

    Added Global decorator to SharedModule

--- NestDemo.Server/src/shared/shared.module.ts
+++ NestDemo.Server/src/shared/shared.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
 import { MapperService } from './mapping/mapper.service';
 
+/**
+ * *Global decorator allows us to provide SharedModule on a global scope with only importing it in AppModule
+ */
+@Global()
 @Module({
     providers: [ConfigService, MapperService],
     exports: [ConfigService, MapperService],
